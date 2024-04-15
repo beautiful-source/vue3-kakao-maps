f17d18bec8ca4c82ae8b4b9a490ba4a8276e629d
Kihyun Ryu <112453419+geekseal@users.noreply.github.com>
2024-04-15 16:08:58 +0900

    Fix: d.ts 파일 생성하는 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,31 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/main.ts'),
-      formats: ['es'], // 트리쉐이킹을 활용하기 위해 es 포맷 설정
-      name: 'Vue3KakaoMap'
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      // 트리쉐이킹을 활용하기 위해 es 포맷 설정
+      // formats: ['es'],
+      name: 'Ryutest',
+      fileName: (format) => `index.${format}.js`
     },
     rollupOptions: {
-      external: ['vue'], // 최종 빌드에서 Vue 제외
+      // 최종 빌드에서 Vue 제외
+      external: ['vue'],
       output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
         globals: {
-          Vue: 'vue'
+          vue: 'Vue'
         }
       }
     }
   },
-  plugins: [vue(), VueDevTools()],
+  plugins: [vue(), VueDevTools(), dts()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
