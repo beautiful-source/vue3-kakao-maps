f649a63e1a14b9b875ad3bac9e6f387e3345e994
정서희 <jsh001505@naver.com>
2024-04-15 16:12:20 +0900

    Fix: 파일명 변경이 반영되지 않은 오류 수정

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,22 +1,22 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
-import { fn } from '@storybook/test';
-import Button from './Button.vue';
+import type { Meta, StoryObj } from "@storybook/vue3";
+import { fn } from "@storybook/test";
+import Button from "./VButton.vue";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
-    size: { control: 'select', options: ['small', 'medium', 'large'] },
-    backgroundColor: { control: 'color' },
+    size: { control: "select", options: ["small", "medium", "large"] },
+    backgroundColor: { control: "color" }
   },
   args: {
     primary: false,
     // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-    onClick: fn(),
-  },
+    onClick: fn()
+  }
 } satisfies Meta<typeof Button>;
 
 export default meta;
@@ -29,27 +29,27 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
   args: {
     primary: true,
-    label: 'Button',
-  },
+    label: "Button"
+  }
 };
 
 export const Secondary: Story = {
   args: {
     primary: false,
-    label: 'Button',
-  },
+    label: "Button"
+  }
 };
 
 export const Large: Story = {
   args: {
-    label: 'Button',
-    size: 'large',
-  },
+    label: "Button",
+    size: "large"
+  }
 };
 
 export const Small: Story = {
   args: {
-    label: 'Button',
-    size: 'small',
-  },
+    label: "Button",
+    size: "small"
+  }
 };
