4ab139de51608129c5709f7340355762811e07dd
김은진 <asd0755@naver.com>
2024-04-15 09:37:40 +0900

    Chore: husky 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, type Component, type ComputedOptions, type MethodOptions } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const TypedAssertedApp = App as Component<any, any, any, ComputedOptions, MethodOptions, any, any>;
+createApp(TypedAssertedApp).mount("#app");
