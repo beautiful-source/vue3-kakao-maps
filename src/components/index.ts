a56a8a386354d65aa18a32098a6f02998acd0783
정서희 <jsh001505@naver.com>
2024-04-22 14:01:46 +0900

    Feature: index.ts에 컴포넌트 export 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 import KakaoMap from './KakaoMap/KakaoMap.vue';
+import MapMarker from './MapMarker/MapMarker.vue';
+import InfoWindow from './InfoWindow/InfoWindow.vue';
 
-export { KakaoMap };
+export { KakaoMap, MapMarker, InfoWindow };
