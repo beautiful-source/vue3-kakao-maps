ae8b65d7258c774a87cd39a1d780dd3393c11094
전소민 <goflvhxj96@naver.com>
2024-04-23 14:29:56 +0900

    Fix: addon-docs 의존성 문제 해결

--- src/storybook/MoveCenter/MoveCenter.stories.ts
+++ src/storybook/MoveCenter/MoveCenter.stories.ts
@@ -1,8 +1,9 @@
 import type { StoryObj } from '@storybook/vue3';
+import type { KakaoMapProps } from '@/components/KakaoMap/KakaoMap.vue';
 import { KakaoMap } from '@/components';
 import { ref } from 'vue';
+import useKakao from '@/util/useKakao';
 
-import type { KakaoMapProps } from '@/components/KakaoMap/KakaoMap.vue';
 const meta = {
   title: 'Components/MoveCenter',
   parameters: {
@@ -13,30 +14,31 @@ const meta = {
 
 export default meta;
 
-const renderKakaoMap: any = (args: KakaoMapProps) => ({
+const renderMoveCenter: any = (args: KakaoMapProps) => ({
   components: { KakaoMap },
   setup() {
-    const curlat = ref(33.450701);
-    const curlng = ref(126.570667);
+    useKakao(import.meta.env.VITE_KAKAO_APP_KEY ?? '');
+    const curLat = ref(33.450701);
+    const curLng = ref(126.570667);
 
-    const move = (newlat: number, newlng: number): void => {
-      curlat.value = newlat;
-      curlng.value = newlng;
+    const move = (newLat: number, newLng: number): void => {
+      curLat.value = newLat;
+      curLng.value = newLng;
     };
 
     return {
-      curlat,
-      curlng,
+      curLat,
+      curLng,
       move
     };
   },
   template: `
-    <KakaoMap app-key="${import.meta.env.VITE_KAKAO_APP_KEY}" :lat="curlat" :lng="curlng"></KakaoMap>
+    <KakaoMap :lat="curLat" :lng="curLng"></KakaoMap>
     <button @click="move(33.452613, 126.570888)">moveTo1</button>
     <button @click="move(33.45058, 126.574942)">moveTo2</button>
   `
 });
 
-export const Move: StoryObj = {
-  render: renderKakaoMap
+export const MoveCenter: StoryObj = {
+  render: renderMoveCenter
 };
