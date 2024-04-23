50d2e975bd89385dde1f9397a3dd396f53026a76
전소민 <goflvhxj96@naver.com>
2024-04-23 14:41:06 +0900

    Fix: addon-docs 의존성 문제 해결

--- src/components/KakaoMap/KakaoMap.stories.ts
+++ src/components/KakaoMap/KakaoMap.stories.ts
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
 import { KakaoMap } from '@/components';
 import type { KakaoMapProps } from './KakaoMap.vue';
+import useKakao from '@/util/useKakao';
 
 const meta = {
   title: 'Components/KakaoMap',
@@ -8,51 +9,7 @@ const meta = {
   parameters: {
     componentSubtitle: '카카오 기본 지도 컴포넌트입니다.'
   },
-  tags: ['autodocs'],
-  argTypes: {
-    width: {
-      description: '맵의 가로 길이입니다. number로 설정할 시 단위는 px로 결정됩니다.',
-      table: {
-        type: { summary: 'number | string' },
-        defaultValue: { summary: '40rem' }
-      }
-    },
-    height: {
-      description: '맵의 세로 길이입니다. number로 설정할 시 단위는 px로 결정됩니다.',
-      table: {
-        type: { summary: 'number | string' },
-        defaultValue: { summary: '50rem' }
-      }
-    },
-    lat: {
-      description: '초기 center 위도 좌표입니다.',
-      table: {
-        type: { summary: 'number' },
-        defaultValue: { summary: 37.566826 }
-      }
-    },
-    lng: {
-      description: '초기 center 경도 좌표입니다.',
-      table: {
-        type: { summary: 'number' },
-        defaultValue: { summary: 126.9786567 }
-      }
-    },
-    markerList: {
-      description: '맵 내부에 표시될 marker 배열입니다.',
-      table: {
-        type: { summary: 'any' },
-        defaultValue: { summary: [] }
-      }
-    },
-    draggable: {
-      description: '지도를 드래그 가능한지 여부입니다.',
-      table: {
-        type: { summary: 'boolean' },
-        defaultValue: { summary: true }
-      }
-    }
-  }
+  tags: ['autodocs']
 } satisfies Meta<typeof KakaoMap>;
 
 export default meta;
@@ -61,6 +18,7 @@ type Story = StoryObj<typeof KakaoMap>;
 const renderKakaoMap: any = (args: KakaoMapProps) => ({
   components: { KakaoMap },
   setup() {
+    useKakao(import.meta.env.VITE_KAKAO_APP_KEY ?? '');
     return args;
   },
   template: `
