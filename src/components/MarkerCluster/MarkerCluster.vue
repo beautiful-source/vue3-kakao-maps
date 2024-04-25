f226db8ab2b45f201fceba272a1873afa2888648
전소민 <goflvhxj96@naver.com>
2024-04-25 16:06:52 +0900

    Feature: Marker Cluster type 추가

--- src/components/MarkerCluster/MarkerCluster.vue
+++ src/components/MarkerCluster/MarkerCluster.vue
@@ -2,12 +2,86 @@
 import { isKakaoMapApiLoaded } from '@/util/useKakao';
 import { ref, watch } from 'vue';
 
-const props = defineProps<{
-  map: kakao.maps.Map;
-  markerList: kakao.maps.Marker[] | undefined;
-}>();
+/**
+ * MarkerCluster 컴포넌트 생성을 위한 타입
+ */
+export type MarkerClusterProps = {
+  /**
+   * 클러스터링 마커를 표시할 지도 객체
+   */
+  map?: kakao.maps.Map;
+
+  /**
+   * 클러스터링 할 마커 배열
+   */
+  markers?: Array<kakao.maps.Marker | kakao.maps.CustomOverlay>;
+
+  /**
+   * 클러스터의 격자 크기 (기본값: 60)
+   */
+  gridSize?: number;
+
+  /**
+   * 마커들의 좌표 평균을 클러스터 좌표 설정 여부 (기본값 : false)
+   */
+  averageCenter?: boolean;
+
+  /**
+   * 클러스터링 할 지도의 최소 레벨 값 (기본값: 0)
+   */
+  minLevel?: number;
+
+  /**
+   * 클러스터링 할 최소 마커 수 (기본값: 2)
+   */
+  minClusterSize?: number;
+
+  /**
+   * 클러스터의 스타일
+   */
+  styles?: object[];
+
+  /**
+   * 클러스터에 표시할 문자열 또는 문자열 생성 함수.
+   * @default "클러스터에 포함된 숫자"
+   */
+
+  texts?: string[] | ((size: number) => string);
+  /**
+   * 클러스터 크기를 구분하는 값을 가진 배열 또는 구분값 생성함수
+   * @default [10, 100, 1000, 10000]
+   */
+  calculator?: number[] | ((size: number) => number[]);
+
+  /**
+   * 클러스터 클릭 시 지도 확대 여부 (기본값: true)
+   */
+  disableClickZoom?: boolean;
+
+  /**
+   * 클러스터 클릭 가능 여부 지정 옵션 (기본값: true)
+   */
+  clickable?: boolean;
+
+  /**
+   * 클러스터에 마우스 over/out 가능 여부 지정 옵션 (기본값: true)
+   */
+  hoverable?: boolean;
+};
+
+const props = defineProps<MarkerClusterProps>();
 
 const clusterer = ref<null | kakao.maps.MarkerClusterer>(null);
+
+const initCluster = (): void => {
+  if (props.markers === undefined) {
+    throw new Error('MarkerList가 없습니다.');
+  }
+  clusterer.value = new kakao.maps.MarkerClusterer({
+    map: props.map,
+    markers: props.markers
+  });
+};
 
 watch(
   () => isKakaoMapApiLoaded.value,
@@ -17,16 +91,6 @@ watch(
     }
   }
 );
-
-const initCluster = (): void => {
-  if (props.markerList === undefined) {
-    throw new Error('MarkerList가 없습니다.');
-  }
-  clusterer.value = new kakao.maps.MarkerClusterer({
-    map: props.map,
-    markers: props.markerList
-  });
-};
 </script>
 
 <template>
