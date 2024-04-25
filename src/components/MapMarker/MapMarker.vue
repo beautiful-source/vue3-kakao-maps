cb2ba30bd32ab46f22a9b87023a656036a276d27
정서희 <jsh001505@naver.com>
2024-04-25 09:53:19 +0900

    Feature: Map에서 하위 컴포넌트로 map 객체 내려보내기

--- src/components/MapMarker/MapMarker.vue
+++ src/components/MapMarker/MapMarker.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts">
 import { isKakaoMapApiLoaded } from '@/util/useKakao';
-import { onBeforeUnmount, ref, watch, onMounted } from 'vue';
-import type { MapMarkerProps } from './types';
+import { onBeforeUnmount, ref, watch, onMounted, inject, type Ref } from 'vue';
+/**
+ * MapMarker 컴포넌트 생성을 위한 타입
+ */
+export type MapMarkerProps = {
+  /**
+   * 마커의 위도 값
+   */
+  lat: number;
+  /**
+   * 마커의 경도 값
+   */
+  lng: number;
+  /**
+   * 마커에 추가할 InfoWindow content
+   */
+  infoWindow?: string;
+};
 
 const props = defineProps<MapMarkerProps>();
-
+// kakao api로 생성한 marker 객체
 const marker = ref<null | kakao.maps.Marker>(null);
-const markerElement = ref<HTMLDivElement>();
+// 마커가 위치할 지도의 위도
 const lat = ref<number>(props.lat);
+// 마커가 위치할 지도의 경도
 const lng = ref<number>(props.lng);
+// 마커가 표시될 지도의
+const mapRef = inject<Ref<kakao.maps.Map>>('mapRef');
 
-watch(
-  () => isKakaoMapApiLoaded.value,
-  (isKakaoMapApiLoaded) => {
-    if (isKakaoMapApiLoaded) {
-      initMarker(props.map);
-    }
+watch([() => isKakaoMapApiLoaded.value, () => mapRef], ([isKakaoMapApiLoaded, mapRef]) => {
+  if (isKakaoMapApiLoaded && mapRef !== undefined) {
+    initMarker(mapRef.value);
   }
-);
+});
 
 onMounted(() => {
-  if (isKakaoMapApiLoaded.value) {
-    initMarker(props.map);
+  if (isKakaoMapApiLoaded.value && mapRef !== undefined) {
+    initMarker(mapRef.value);
   }
 });
 onBeforeUnmount(() => {
@@ -42,7 +58,7 @@ const initMarker = (map: kakao.maps.Map): void => {
 </script>
 
 <template>
-  <div ref="markerElement">
+  <div>
     <slot></slot>
   </div>
 </template>
