d2c321977fb3ac461141fd30596607127ad9443b
류기현 <112453419+geekseal@users.noreply.github.com>
2024-04-25 12:33:34 +0900

    Chore: 마커 props 타입 모두 추가 및 불필요한 watch 삭제

--- src/components/MapMarker/MapMarker.vue
+++ src/components/MapMarker/MapMarker.vue
@@ -18,6 +18,44 @@ export type MapMarkerProps = {
    * 마커에 추가할 InfoWindow content
    */
   infoWindow?: string;
+  /**
+   * 마커의 이미지
+   */
+  // TODO: 마커 이미지 타입 추가
+  /**
+   * 마커의 타이틀 속성 값 (툴팁)
+   */
+  title?: string;
+
+  /**
+   * 마커의 드래그 가능 여부
+   */
+  draggable?: boolean;
+
+  /**
+   * 마커의 클릭 가능 여부
+   */
+  clickable?: boolean;
+
+  /**
+   * 마커의 z-index 속성 값
+   */
+  zIndex?: number;
+
+  /**
+   * 마커 투명도 (0-1)
+   */
+  opacity?: number;
+
+  /**
+   * 로드뷰에 올라가 있는 마커의 높이 값(m 단위)
+   */
+  altitude?: number;
+
+  /**
+   * 로드뷰 상에서 마커의 가시반경(m 단위), 두 지점 사이의 거리가 지정한 값보다 멀어지면 마커는 로드뷰에서 보이지 않게 됨
+   */
+  range?: number;
 };
 
 const props = defineProps<MapMarkerProps>();
@@ -53,14 +91,6 @@ watch(
 
 onBeforeUnmount(() => {
   marker.value?.setMap(null); // 컴포넌트 삭제될 때 map에서 marker 삭제
-});
-
-/**
- * map 변경감지
- */
-watch([() => mapRef], ([newMap]) => {
-  // TODO: provide/inject 후 사라질 예정입니다.
-  console.log('new map', newMap);
 });
 
 /**
