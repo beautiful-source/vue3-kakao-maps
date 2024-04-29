65b4806e335e13d0fdd555388ec5a8212453e43a
양소영 <maysoyoung@gmail.com>
2024-04-29 13:23:21 +0900

    Refactor: 스토리북 리팩토링

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,13 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
+    '@storybook/addon-docs'
+  ],
   framework: {
     name: '@storybook/vue3-vite',
     options: {}
